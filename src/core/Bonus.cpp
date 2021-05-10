714e1255e09a36c2969ffedb929e6b66317ae7df
Aymeric LETO <aymeric.leto@etu.univ-lyon1.fr>
2021-05-10 14:22:04 +0200

    R

---
+++ src/core/Bonus.cpp
@@ -0,0 +1,21 @@
+/**
+@brief Implémentation de la classe serpent
+
+@file Serpent.cpp
+@author : Aymeric Leto, Benoît Briguet, Nathan Puricelli
+@date : Mars 2021
+*/
+
+#include "Bonus.h"
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
+
+const float PI = 3.1415926535;
+
+using namespace std;
+
--- src/core/Bonus.h
+++ src/core/Bonus.h
@@ -7,21 +7,23 @@ Le module Serpent permet de gérer un serpent et ses informations.
 @author : Aymeric Leto, Benoît Briguet, Nathan Puricelli
 @date : Mars 2021
 */
-#ifndef SERPENT_H
-#define SERPENT_H
+#ifndef BONUS_H
+#define BONUS_H
 
 #include <iostream>
 #include <string>
-#include "Couleur.h"
 #include "Terrain.h"
 
 using namespace std;
 
-class Serpent
+class Bonus
 {
 private :
-    
+    float x, y;
+    bool estApparu;
 public :
+    Bonus();
+    ~Bonus();
 
 };
 
