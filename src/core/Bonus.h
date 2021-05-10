8639b2724503a07eeff31c1f070c17b26f357b5f
Aymeric LETO <aymeric.leto@etu.univ-lyon1.fr>
2021-05-10 14:14:09 +0200

    Chantier

---
+++ src/core/Bonus.h
@@ -0,0 +1,28 @@
+/**
+@brief Module gérant les serpents.
+
+Le module Serpent permet de gérer un serpent et ses informations.
+
+@file Serpent.h
+@author : Aymeric Leto, Benoît Briguet, Nathan Puricelli
+@date : Mars 2021
+*/
+#ifndef SERPENT_H
+#define SERPENT_H
+
+#include <iostream>
+#include <string>
+#include "Couleur.h"
+#include "Terrain.h"
+
+using namespace std;
+
+class Serpent
+{
+private :
+    
+public :
+
+};
+
+#endif
